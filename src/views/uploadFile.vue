fe8a725137a230527cd17815f9d9072ecf03982d
zhangwenchao19 <zhangwenchao19@jd.com>
2019-10-24 20:21:33 +0800

    文件上传提交

--- src/views/uploadFile.vue
+++ src/views/uploadFile.vue
@@ -2,7 +2,7 @@
   <div class="bank-box">
     <el-row>
       <el-col :span="3">
-        <el-input v-model="input" placeholder="请输入文件名称"></el-input>
+        <el-input v-model="bank.name" placeholder="请输入文件名称"></el-input>
       </el-col>
       <el-col :span="3">
         <el-date-picker
@@ -14,7 +14,7 @@
         ></el-date-picker>
       </el-col>
       <el-col :span="2" :offset="4">
-        <el-button type="primary" icon="el-icon-search" @click="search()">搜索</el-button>
+        <el-button type="primary" icon="el-icon-search" @click="getBanks()">搜索</el-button>
       </el-col>
       <el-col :span="2" :offset="4">
         <el-button type="primary" icon="el-icon-circle-plus-outline" @click="handleAdd">上传文件</el-button>
@@ -38,7 +38,6 @@
       :title="dialogTitle"
       width="600px"
       :visible.sync="bankFormVisible"
-      @close="resetForm('bankForm')"
     >
       <el-upload
         class="upload-demo"
@@ -53,16 +52,11 @@
         </div>
         <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
       </el-upload>
-      <div slot="footer" class="dialog-footer">
-        <el-button @click="bankFormVisible = false">取 消</el-button>
-        <el-button type="primary" @click="submitBank('bankForm')">确 定</el-button>
-      </div>
     </el-dialog>
   </div>
 </template>
 
 <script>
-import searchs from "@/components/search/search";
 export default {
   data() {
     return {
@@ -92,9 +86,6 @@ export default {
   mounted() {
     this.getBanks();
   },
-  components: {
-    searchs
-  },
   props: ["param"],
   methods: {
     getBanks(param) {
@@ -122,65 +113,11 @@ export default {
           console.error(err);
         });
     },
-    investmentDetails(index, row) {
-      this.$message({
-        showClose: true,
-        message: "将来展示投资明细"
-      });
-    },
     submitBank(formName) {
       // 表单验证
       this.$refs[formName].validate(valid => {
-        if (valid) {
-          let id = this.bank.id;
-          if (id) {
-            // id非空-修改
-            this.banks.splice(this.rowIndex, 1, this.bank);
-          } else {
-            // id为空-新增
-            this.bank.id = this.banks.length + 1;
-            this.banks.unshift(this.bank);
-          }
-          this.bankFormVisible = false;
-          this.$message({
-            type: "success",
-            message: id ? "修改成功！" : "新增成功！"
-          });
-        }
+        console.log(formName);
       });
-    },
-    operationLog(index, row) {
-      this.$message({
-        showClose: true,
-        message: "将来展示交易流水"
-      });
-    },
-    resetForm(formName) {
-      this.$refs[formName].clearValidate();
-    },
-    mulDelete() {
-      let len = this.multipleSelection.length;
-      if (len === 0) {
-        this.$message({
-          type: "warning",
-          message: "请至少选择一项！"
-        });
-      } else {
-        this.$confirm(`确定删除选中的 ${len} 个用户吗？`, "提示", {
-          confirmButtonText: "确定",
-          cancelButtonText: "取消",
-          type: "warning"
-        })
-          .then(() => {
-            this.$message({
-              type: "success",
-              message: `成功删除了${len}条数据！`
-            });
-          })
-          .catch(() => {
-            console.log("取消删除");
-          });
-      }
     },
     selectChange(val) {
       this.multipleSelection = val;
