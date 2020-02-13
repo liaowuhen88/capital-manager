cd9b5add3b70311e0a2b15bcbe1c90aa83609b7f
zhangwenchao19 <zhangwenchao19@jd.com>
2020-02-13 16:38:57 +0800

    tijiao

--- src/views/expectedIncomeTotal.vue
+++ src/views/expectedIncomeTotal.vue
@@ -2,23 +2,46 @@
   <div>
     <el-row>
       <el-col :span="5">
-        <el-date-picker v-model="param.startTime" type="date" placeholder="开始日期" value-format="yyyy-MM-dd"></el-date-picker>
+        <el-date-picker
+          v-model="param.startTime"
+          type="month"
+          placeholder="开始日期"
+          value-format="yyyy-MM"
+        ></el-date-picker>
       </el-col>
-       <el-col :span="1">
-         <p>
+      <el-col :span="1">
+        <p>
           <font size="4" face="arial">至</font>
         </p>
       </el-col>
       <el-col :span="5">
-        <el-date-picker v-model="param.endTime" type="date" placeholder="结束日期" value-format="yyyy-MM-dd"></el-date-picker>
+        <el-date-picker
+          v-model="param.endTime"
+          type="month"
+          placeholder="结束日期"
+          value-format="yyyy-MM"
+        ></el-date-picker>
       </el-col>
       <el-col :span="5" :offset="5">
         <el-button type="primary" icon="el-icon-search" @click="search()">搜索</el-button>
       </el-col>
     </el-row>
-    <div class="echarts-box">
+
+  <div class="echarts-box">
       <div class="echarts" id="echarts"></div>
     </div>
+
+    <el-table
+      :data="totalByMonthTableVo"
+      style="width:400px"
+      show-summary
+      :summary-method="getSummaries"
+    >
+      <el-table-column prop="profitDate" label="月份"></el-table-column>
+      <el-table-column :formatter="formatter" prop="expectedInterestIncomeMonth" label="预期收益"></el-table-column>
+    </el-table>
+
+  
   </div>
 </template>
 
@@ -28,6 +51,7 @@ export default {
   data() {
     return {
       totalByMonthVo: {},
+      totalByMonthTableVo: {},
       param: {
         startTime: "",
         endTime: "",
@@ -39,7 +63,9 @@ export default {
   },
   mounted() {
     this.getData();
+    this.getDataTable();
   },
+
   methods: {
     drawEcharts() {
       let chart = this.$echarts.init(document.getElementById("echarts"));
@@ -49,6 +75,34 @@ export default {
       option.legend = this.totalByMonthVo.legend;
 
       chart.setOption(option);
+    },
+    formatter(row, column) {
+      return this.Utils.toMoney(row[column.property]) + "元";
+    },
+    getSummaries(param) {
+      const { columns, data } = param;
+      const sums = [];
+      columns.forEach((column, index) => {
+        if (index === 0) {
+          sums[index] = "总计：";
+          return;
+        }
+        const values = data.map(item => Number(item[column.property]));
+        if (!values.every(value => isNaN(value))) {
+          sums[index] = values.reduce((prev, curr) => {
+            const value = Number(curr);
+            if (!isNaN(value)) {
+              return prev + curr;
+            } else {
+              return prev;
+            }
+          }, 0);
+          sums[index] = this.Utils.toMoney(sums[index]) + "元";
+        } else {
+          sums[index] = "";
+        }
+      });
+      return sums;
     },
     getData(param) {
       this.loading = true;
@@ -72,7 +126,29 @@ export default {
           console.error(err);
         });
     },
+    getDataTable(param) {
+      this.loading = true;
+      this.$http({
+        method: "post",
+        url: this.BASE_API + "/api/bankMyProducts/expectedIncomeTotalTable",
+        data: param ? param : {}
+      })
+        .then(res => {
+          if (res.data.code == 0) {
+            this.totalByMonthTableVo = res.data.data;
+          } else {
+            this.$message({
+              showClose: true,
+              message: res.data.msg
+            });
+          }
+        })
+        .catch(err => {
+          console.error(err);
+        });
+    },
     search() {
+      this.getDataTable(this.param);
       this.getData(this.param);
     }
   }
